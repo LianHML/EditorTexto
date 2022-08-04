808d1473fdbe2fb61112598069e2808a9df1997f
Lian <lianlinsmeyer@gmail.com>
2022-08-03 22:21:32 -0300

    Criação do projeto para revisão de princípios C#

---
+++ EditorTexto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EditorTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
