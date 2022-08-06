52e9e52a01212dbf1d8f1f782d570d7772e96114
Lian <lianlinsmeyer@gmail.com>
2022-08-06 10:39:13 -0300

    Blocagem do menu

--- EditorTexto/Program.cs
+++ EditorTexto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace EditorTexto
 {
@@ -6,7 +7,41 @@ namespace EditorTexto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
         }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Editor de Texto");
+            Console.WriteLine("");
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Abrir Arquivo");
+            Console.WriteLine("2 - Criar Novo Arquivo");        
+            Console.WriteLine("0 - Sair");
+            short opcaoSelecionada = short.Parse(Console.ReadLine());
+
+            switch(opcaoSelecionada)
+            {
+                case 0: Sair(); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+            }
+        }
+
+        static void Abrir()         
+        {
+        
+        }
+        static void Editar() 
+        {
+
+        }
+        static void Sair()
+        {
+
+        }
+
     }
+
 }
